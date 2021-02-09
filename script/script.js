0d0ce7d9623cee5f8a6dee0b631e226ccf4105af
unknown <C:\The Bat!>
2021-02-09 18:49:35 +0200

    final

--- script/script.js
+++ script/script.js
@@ -8,7 +8,19 @@ const control=document.querySelector("#modalFooter");
 const next=document.querySelector("#next");
 const prev=document.querySelector("#prev");
 const send=document.querySelector("#send");
+var firebaseConfig = {
+						apiKey: "AIzaSyD90Z8q06bxdpLjWX9HtkmrxpmAeu9i_f0",
+						authDomain: "quiz-b9e53.firebaseapp.com",
+						databaseURL: "https://quiz-b9e53-default-rtdb.firebaseio.com",
+						projectId: "quiz-b9e53",
+						storageBucket: "quiz-b9e53.appspot.com",
+						messagingSenderId: "488640005104",
+						appId: "1:488640005104:web:3c9835297fa64f927b9335",
+						measurementId: "G-WP2MNRB6QJ"
+						  };
 
+						  
+						  
 let qnumber=0;
 		
 	btn.addEventListener('click',( )=>{
@@ -20,17 +32,15 @@ let qnumber=0;
 	const obj={};
 	const getData=()=>{
 		formAnswers.textContent='LOAD';
-		
-		
-		setTimeout(()=>{
+		next.style.display="none";
+		prev.style.display="none";
+		send.style.display="none";
+		setTimeout(()=>{firebase.initializeApp(firebaseConfig);
+		next.style.display="block";
+			firebase.database().ref().child('questions').once('value')
+						  .then(snap=>playTest(snap.val()))		
 			formAnswers.textContent='';
-			fetch('./questions.json')
-				.then(res=>res.json())
-				.then(obj=>playTest(obj.questions))
-				.catch(err=>{
-					formAnswers.textContent="Data error";
-					console.error(err)})
-			console.log(obj.questions);
+
 		},1000)
 	}
 	
@@ -61,15 +71,30 @@ let qnumber=0;
 		})
 		
 		next.addEventListener('click',( )=>{
+			checkAnsw();
 			answ.innerHTML=``;
 			block.classList.remove('d-block');
 			qnumber++;
 			block.classList.add('d-block')
 			//if(qnumber<questions.length){
 			render(qnumber);
-			fillansw();
+			//fillansw();
 		})
-	
+		const checkAnsw=()=>{
+			const obj={};
+			
+			const inputs = [...formAnswers.elements].filter((input)=>input.checked||input.id==="numberPhone");
+			inputs.forEach((input,index)=>{
+				if(qnumber<4)
+				obj[`${index}_${questions[qnumber].question}`]=input.value;
+				else{
+				obj['numPhone']=input.value;
+				}
+			})
+			console.log(obj);
+			finalanswers.push(obj);
+			console.log(finalanswers);
+		}
 		prev.addEventListener('click',( )=>{
 			answ.innerHTML=``;
 			block.classList.remove('d-block');
@@ -79,40 +104,59 @@ let qnumber=0;
 		})
 		
 		send.addEventListener('click',()=>{
-			fillansw();
-			console.log(finalanswers);
+			checkAnsw();
+			firebase
+			.database()
+			.ref()
+			.child('contacts')
+			.push(finalanswers)
 		})
 		
 		const render=(number)=>{
 			console.log(number);
-			que.textContent=questions[qnumber].question;
+			if(number<4){
+				que.textContent=questions[qnumber].question;
 			console.log(questions[qnumber]);
 			questions[qnumber].answers.forEach(i=>{
 			answ.innerHTML+=`
 			<div class="answers-item d-flex flex-column-center">
                 <input type="${questions[qnumber].type}" id="${i.title}" name="answer" class="d-none" value="${i.title}">
-                <label for="answerItem1" class="d-flex flex-column justify-content-between">
+                <label for="${i.title}" class="d-flex flex-column justify-content-between">
                   <img class="answerImg" src=${i.url} alt="burger">
                   <span>${i.title}</span>
                 </label>
               </div>`
-			  
+			console.log(questions[qnumber].type);  
+			send.style.display="none";
 			})
-			console.log(questions[qnumber].type);
-			switch(qnumber){
-				case 0:prev.style.display="none";
-					break;
-				case 4:next.style.display="none";
-				prev.style.display="none";
-				formAnswers.innerHTML=`
+			}
+			else{
+
+			  formAnswers.innerHTML=`
 					<div class="form-group">
 						<label for="numberPhone">Enter your number</label>
 						<input type="phone" class="form-control" id="numberPhone">
 					<div>
 				`;
+				
+			}
+			
+			
+			switch(number){
+				case 0:prev.style.display="none";
+					break;
+				case 4://next.style.display="none";
+				prev.style.display="none";next.style.display="block";
+				send.style.display="block";
 				send.classList.remove='d-none';
+				que.textContent='Заполните форму';
 				break;
-				case 5:formAnswers.textContent="Thank you";
+				case 5:
+				que.textContent='Спасибо за прохождение';
+				prev.style.display="block";
+				next.style.display="none";
+				send.style.display="none";
+				formAnswers.textContent="Thank you";
 				setTimeout(()=>{
 					block.classList.remove('d-block');
 				},2000);
